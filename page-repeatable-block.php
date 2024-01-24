5e4496d9f59032e3b71cf28901133bbb4e431860
accrane <austincrane@gmail.com>
2024-01-24 14:15:08 -0500

    add styling

--- page-repeatable-block.php
+++ page-repeatable-block.php
@@ -66,17 +66,8 @@ if($soon !== 'soon') :
             <?php if ( $gallery ) { ?>
             <div class="imagecol block">
               <?php foreach( $gallery as $image ) { ?>
-                <div class="polaroid polmov" >
-                  <div class="p-pic">
-                    <div class="tape"></div>
-                      <img src="<?php bloginfo('template_url'); ?>/images/Polaroid-Frame.png">
-                    </div>
-                    <div class="behind-polaroid">
-                      <img src="<?php echo $image['url'] ?>" alt=" <?php echo $image['title'] ?>">
-                    </div>
-                  
-                  
-                  
+                <div class="imagediv" >
+                  <img src="<?php echo $image['url'] ?>" alt=" <?php echo $image['title'] ?>">
                 </div>
               <?php } ?>
             </div> 
