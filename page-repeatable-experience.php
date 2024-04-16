7fba908099f6f575ccaed14f88d2d3285c71f2d6
accrane <austincrane@gmail.com>
2024-04-16 14:32:42 -0400

    add images

--- page-repeatable-experience.php
+++ page-repeatable-experience.php
@@ -13,6 +13,41 @@ if($soon !== 'soon') :
 <?php get_template_part('parts/hero-subpage'); ?>
 <div id="primary" class="content-area default-template">
 
+
+	<?php 
+	$blocks = get_field('repeatable_block'); 
+	// echo '<pre>';
+	// print_r($blocks);
+	// echo '</pre>';
+	
+	
+	?>
+	<section class="anchors">
+    <ul>
+      <li>//</li>
+      <?php foreach( $blocks as $b ) { 
+      		$secTitle = $b['title'];
+			if( $secTitle ){
+				$santi = sanitize_title_with_dashes($secTitle);
+			}
+      	?>
+      	<li>
+      		<a href="#<?php echo $santi; ?>"><?php echo $secTitle; ?></a>
+      	</li>
+      <?php } ?>
+      <!-- <li>
+        <a href="#pro-comps">Pro Comps</a>
+      </li>
+      <li>
+        <a href="#citizen-comps">Citizen Comps</a>
+      </li>
+      <li>
+        <a href="#all-comps">All Comps</a>
+      </li> -->
+      <li>//</li>
+    </ul>
+  </section>
+
   <main id="main" class="site-main">
     <?php while ( have_posts() ) : the_post(); ?>
       <?php //if ( get_the_content() ) { ?>
@@ -30,22 +65,24 @@ if($soon !== 'soon') :
         <?php $n=1; while ( have_rows('repeatable_block') ) : the_row(); 
           $image_above_title = get_sub_field('image_above_title');
           $title = get_sub_field('title');
+          $sani = sanitize_title_with_dashes($title);
           $text = get_sub_field('text');
           $buttons = get_sub_field('buttons');
           $gallery = get_sub_field('gallery');
           $column_class = ( ($title || $text) &&  $gallery ) ? 'half':'full';
           if( ($title || $text) ||  $gallery ) { ?>
-          <div class="content-block <?php echo $column_class ?>">
-          	<?php if( $image_above_title ) { ?>
-          		<div class="image-above">
-          			<img src="<?php echo $image_above_title['url']; ?>">
-          		</div>
-          	<?php } ?>
+          <div class="content-block <?php echo $column_class ?>"  >
+          	
             <?php if ( $title || $text ) { ?>
             <div class="textcol block">
               <div class="inside">
+              	<?php if( $image_above_title ) { ?>
+	          		<div class="image-above">
+	          			<img src="<?php echo $image_above_title['url']; ?>">
+	          		</div>
+	          	<?php } ?>
                 <?php if ($title) { ?>
-                 <h2 class="rb_title"><span><b><?php echo $title ?></b></span></h2> 
+                 <h2 class="rb_title" id="<?php echo $sani; ?>"><span><b><?php echo $title ?></b></span></h2> 
                 <?php } ?>
 
                 <?php if ($text) { ?>
